0ef4e26f2deaf7bc9cea8635729aee63df79da2c
Philip Ossenkopp <Philip.Ossenkopp@novatec-gmbh.de>
2018-12-22 17:41:39 +0100

    added test agent

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # gym-bubbleshooter
 This repository contains the game bubble shooter as a gym environment.
+Full instructions: https://www.novatec-gmbh.de/en/creating-a-gym-environment/
 Based on: https://github.com/justinmeister/bubbleshooter
---
+++ test_agent.py
@@ -0,0 +1,18 @@
+import gym
+import gym_bubbleshooter
+
+
+def random_agent(episodes=100):
+	env = gym.make("BubbleShooter-v0")
+	env.reset()
+	env.render()
+	for e in range(episodes):
+		action = env.action_space.sample()
+		state, reward, done, _ = env.step(action)
+		env.render()
+		print(reward)
+		if done:
+			break
+
+if __name__ == "__main__":
+    random_agent()
